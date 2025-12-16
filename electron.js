29faae41cf990cc98b24d72fbf19e1a7d283ecd5
uaghazade1 <garderappstore@gmail.com>
2025-12-16 23:10:50 +0300

    fix: remove DevTools opening in production and update build script for preload files

--- electron.js
+++ electron.js
@@ -82,8 +82,6 @@ function createWindow() {
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, 'dist/renderer/index.html'));
-    // Open DevTools in production for debugging (remove this line later)
-    mainWindow.webContents.openDevTools();
   }
 
   mainWindow.on('closed', () => {
